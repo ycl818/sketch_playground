04bc6ab2e292c54e8d2eddc234851f80d7b94539
yichien <kevinyichienlee@gmail.com>
2022-08-26 00:15:32 +0800

    draw circle and rect

--- canvas.js
+++ canvas.js
@@ -1,5 +1,14 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-context.fillStyle = 'blue';
-context.fillRect(100, 100, 100, 100);
+// context.fillStyle = 'blue';
+// context.fillRect(100, 100, 100, 100);
+
+context.lineWidth = 4;
+context.beginPath();
+context.rect(100, 100, 400, 400);
+context.stroke();
+
+context.beginPath();
+context.arc(300, 300, 100, 0, Math.PI * 2);
+context.stroke();
